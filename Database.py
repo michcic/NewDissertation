69fb0f05e904e188f9b1067e634ec9330c27fe19
varenghar <varenghar@users.noreply.github.com>
2018-02-07 18:53:56 +0000

    fullfill - example of how to add new column to database

--- Database.py
+++ Database.py
@@ -1,7 +1,8 @@
 import sqlite3
 import json
+import numpy as np
 
-
+# Adds active region to database
 def add_to_database(ar_id, date, track_id, ar_intensity, coords):
     conn = sqlite3.connect('ar_carrington.db')
     curs = conn.cursor()
@@ -19,6 +20,24 @@ def add_to_database(ar_id, date, track_id, ar_intensity, coords):
 
     conn.commit()
     conn.close()
+
+
+def fullfill_pixel_coords(coords, ar_id):
+    print("fullfill_pixel_coords START")
+    conn = sqlite3.connect('ar_carrington.db')
+    curs = conn.cursor()
+
+    js = json.dumps(coords, cls=Encoder)
+    sql = 'UPDATE ar_test2 SET pixel_coordinates = ? WHERE ar_id = ?'
+
+    task = (js, str(ar_id))
+    curs.execute(sql, task)
+
+    conn.commit()
+    conn.close()
+
+
+
 
 
 def load_from_database(ar_id):
@@ -48,5 +67,39 @@ def delete_from_database():
     conn = sqlite3.connect('ar_carrington.db')
     curs = conn.cursor()
     curs.execute('''DELETE FROM ar_test2 WHERE track_id='11039' ''')
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('ar_carrington.db')
+    curs = conn.cursor()
+    #curs.execute('''ALTER TABLE ar_test2 ADD pixel_coordinates''')
+    #conn.commit()
+    #conn.close()
+
+    c = curs.execute('''PRAGMA table_info(ar_test2)''')
+    for r in c:
+        print(r)
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(Encoder, self).default(obj)
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('ar_carrington.db')
+    curs = conn.cursor()
+    c = curs.execute('''PRAGMA table_info(ar_test2) ''')
+    for r in c:
+        print(r)
     conn.commit()
     conn.close()

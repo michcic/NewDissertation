1ebb1b03d30cf4bcfe57df71b8fc45db3bbe24a9
varenghar <varenghar@users.noreply.github.com>
2018-03-14 12:38:28 +0000

    working filaments so far

--- Filament.py
+++ Filament.py
@@ -1,23 +1,7 @@
-import numpy as np
-import matplotlib as plt
-from astropy.coordinates import SkyCoord
-import sunpy.map
-import matplotlib.pyplot as plt
-from sunpy.coordinates import frames
-import astropy.units as u
-import matplotlib
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-import math
-from descartes import PolygonPatch
-from PIL import Image
-import cv2
-import json
-import sqlite3
 import Database as db
 import ObjectPreparation as prep
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
+import sunpy
+from functools import reduce
 
 
 # Go through array with chain code, convert to carrington, draw contour of shape
@@ -26,101 +10,162 @@ from shapely.geometry.polygon import Polygon
 # startx - x coordinate of chain code start position in pixels
 # starty - y coordinate of chain code start position in pixels
 # return - array with coordinates of the contour of the object
-def get_shapes(chains, startx, starty, filename, fil_id, date):
+def get_shapes(chains, startx, starty, filename, track_id, fl_id, date):
     print("get_shapes() START")
     filename = prep.encode_filename(filename)
     date = prep.encode_date(date)
+    all_track = []
     all_coords_carr = []
     all_contours_pix = []
     counter = 0
+    print("STARTX,", startx, "STARTY", starty)
     # Loop goes through array of chain code
     # and calculates coordinate of each of the pixel of the contour
     for c in chains:
         xpos = startx[counter]  # Starting position of contour
         ypos = starty[counter]
-        f_id = fil_id[counter]
+        t_id = track_id[counter]
+        f_id = fl_id[counter]
         print("ID", f_id)
         file = filename[counter]
-        fil_date = date[counter]
+        fl_date = date[counter]
+
         # Check if exists in database
-        result = db.load_sp_from_database(f_id)
-        if not result == ([], []):
+        result = db.load_fl_from_database(f_id)
+        if not result == ([], [], []):
             print("RESULT NOT NULL")
             # check if object go through the end of map and finish at the beginning
-            broken = (max(result[0][0][0]) - min(result[0][0][0])) > 358
+            broken = (max(result[1][0][0]) - min(result[1][0][0])) > 358
             if not broken:
-                all_coords_carr += result[0]
-                all_contours_pix += (result[1])
+                all_track += result[0]
+                all_coords_carr += result[1]
+                all_contours_pix.append(result[2])
         else:
             print("RESULT NULL")
             # Calculate ar contour in pixel, carrington longitude and latitude
-            fil_pix = get_shape(coords=c, xpos=xpos, ypos=ypos, file=file)
-            print("pix", fil_pix)
-            all_contours_pix.append(fil_pix)
-            #db.add_sunspot_to_database(sp_id=f_id, date=fil_date, carr_coords=[lon, lat], pix_coords=sp_pix)
+            pix, lon, lat = prep.get_shape(coords=c, xpos=xpos, ypos=ypos, file=file)
+            all_contours_pix.append(pix)
+            db.add_fl_to_database(f_id, fl_date, t_id, [lon, lat], pix)
 
-            # broken = max(lon) - min(lon) > 358  # check if object go through the end of map and finish at the beginning
-            # if not broken:
-            #     all_coords_carr.append([lon, lat])
+            broken = max(lon) - min(lon) > 358  # check if object go through the end of map and finish at the beginning
+            if not broken:
+                all_track.append(str(t_id))
+                all_coords_carr.append([lon, lat])
 
         counter += 1
 
-    return all_coords_carr, all_contours_pix
+    print("!!!!", len(all_coords_carr), len(all_contours_pix))
+    mer = merge_id_with_object(all_coords_carr, all_contours_pix, all_track)
+    #carrington_synthesis, pixel_synthesis = make_synthesis(mer)
+
+    return mer
 
 
-def get_shape(coords, xpos, ypos, file):
-    lon = []
-    lat = []
-    ar = []
-    print(xpos)
-    for d in coords:
-        if d == 0:
-            xpos -= 1
-        elif d == 1:
-            xpos -= 1
-            ypos -= 1
-        elif d == 2:
-            ypos -= 1
-        elif d == 3:
-            ypos -= 1
-            xpos += 1
-        elif d == 4:
-            xpos += 1
-        elif d == 5:
-            ypos += 1
-            xpos += 1
-        elif d == 6:
-            ypos += 1
-        elif d == 7:
-            ypos += 1
-            xpos -= 1
+# Creates dictionary where key is track_id of filament
+# and values are tuple of carrington coordinates and
+# pixel coordinates
+def merge_id_with_object(carr_coords, pix_coords, track_id):
+    print("merge_id_with_ar START")
+    fl_with_id = {}
+    fl_with_id[track_id[0]] = [(carr_coords[0], pix_coords[0])]
+    if len(carr_coords) == len(track_id):
+        for x in range(1, len(track_id)):
+            if track_id[x] in fl_with_id:
+                fl_with_id[track_id[x]].append((carr_coords[x], pix_coords[x]))
+            else:
+                fl_with_id[track_id[x]] = [(carr_coords[x], pix_coords[x])]
 
-        ar.append([xpos, ypos])
+    return fl_with_id
 
 
-    print("AR", ar)
+# # coord - coordinates of contour of ar
+# # filename - FITS file associated with that ar
+# def calculate_fl_average_intensity(coord, filename):
+#     from ActiveRegion import get_contour_pixels_indexes
+#     print("calculate_fl_average_intensity() START ")
+#     filename = "images//" + filename
+#     coord = get_contour_pixels_indexes(coord, filename)  # find all pixels inside the contour
+#     pixels_number = len(coord)
+#     pixels = []
+#     map = sunpy.map.Map(filename)
+#     # calculate intensity
+#     for x in range(0, pixels_number):
+#         pixels.append(map.data[coord[0][x]][coord[1][x]])
+#
+#     average = reduce(lambda x, y: x + y, pixels) / len(pixels)  # calculate average instensity of pixels
+#
+#     return average
 
-    return ar
+
+
 
 
 
 if __name__ == '__main__':
     from DataAccess import DataAccess
 
-    data = DataAccess('2003-09-26T00:00:00', '2003-09-27T00:00:00', 'FIL')
+    data = DataAccess('2003-09-27T00:00:00', '2003-09-29T00:00:00', 'FIL')
 
     chain_encoded = prep.encode_and_split(data.get_chain_code())
 
-    carr_synthesis, pix = get_shapes(chain_encoded, data.get_pixel_start_x(), data.get_pixel_start_y(),
-                                     data.get_filename(), data.get_fil_id(), data.get_date())
+    mer = get_shapes(chain_encoded, data.get_pixel_start_x(), data.get_pixel_start_y(),
+                                     data.get_filename(), data.get_track_id(), data.get_fil_id(), data.get_date())
 
-    npa = np.array([pix[0]], dtype=np.int32)
-    print(npa)
+    carrington = []
 
-    im = Image.new('RGB', (1200, 1200), (255, 255, 255))
+    for id, coords in mer.items():
+        carrington.append(coords[0][0])
 
-    cv_image = np.array(im)  # convert PIL image to opencv image
-    cv2.fillPoly(cv_image, pts=npa, color=(0, 0, 0))
+    print(carrington)
 
-    cv2.imshow("", cv_image)
-    cv2.waitKey(0)
+    prep.display_object(carrington, "")
+
+
+
+    # npa = np.array([pix[0]], dtype=np.int32)
+    # npa2 = np.array([pix[1]], dtype=np.int32)
+    # print(npa)
+    #
+    # im = Image.new('RGB', (1200, 1200), (255, 255, 255))
+    #
+    # cv_image = np.array(im)  # convert PIL image to opencv image
+    # cv2.fillPoly(cv_image, pts=npa, color=(0, 0, 0))
+    # cv2.fillPoly(cv_image, pts=npa2, color=(0, 0, 0))
+    #
+    # cv2.imshow("", cv_image)
+    # cv2.waitKey(0)
+
+    # def get_shape(coords, xpos, ypos, file):
+
+    # lon = []
+    #     lat = []
+    #     ar = []
+    #     print(xpos)
+    #     for d in coords:
+    #         if d == 0:
+    #             xpos -= 1
+    #         elif d == 1:
+    #             xpos -= 1
+    #             ypos -= 1
+    #         elif d == 2:
+    #             ypos -= 1
+    #         elif d == 3:
+    #             ypos -= 1
+    #             xpos += 1
+    #         elif d == 4:
+    #             xpos += 1
+    #         elif d == 5:
+    #             ypos += 1
+    #             xpos += 1
+    #         elif d == 6:
+    #             ypos += 1
+    #         elif d == 7:
+    #             ypos += 1
+    #             xpos -= 1
+    #
+    #         ar.append([xpos, ypos])
+    #
+    #
+    #     print("AR", ar)
+    #
+    #     return ar

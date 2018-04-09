2ec5affa062d99d417541219ffeb1e0e8cf9767b
varenghar <varenghar@users.noreply.github.com>
2018-04-09 16:57:28 +0100

    interface test

--- Interface.py
+++ Interface.py
@@ -1,16 +1,13 @@
 import tkinter as tk
 from tkinter import ttk, StringVar
-from tkinter.messagebox import showinfo
+from tkinter import messagebox
 from DataAccess import DataAccess
 import ObjectPreparation as prep
 import ActiveRegion as ar
+import Sunspot as sp
 
 
-
-'''Login window from where user can login or go
-to register window'''
-
-
+# Class represents the interface of the software
 class MainFrame(tk.Frame):
 
     def __init__(self, parent):
@@ -43,27 +40,57 @@ class MainFrame(tk.Frame):
         self.fil_instruments = StringVar()
         fil_combo = ttk.Combobox(parent, textvariable=self.fil_instruments)
         fil_combo.grid(row=5, column=1, sticky='W', padx=10)
-        fil_combo['values'] = ('Meudon_H_Alpha_Spectroheliograph')
+        fil_combo['values'] = ('MEUDON/SPECTTROHELIOGRAPH')
 
+        # Function is called after clicking 'Create Map' button
+        # It gathers inputs from fields and create map
+        # If some of the values are empty it throws error
         def create():
+            # get start and end date of observation
             start = self.start_val.get()
             end = self.end_val.get()
-            instrument = self.ar_instruments.get()
-            create_map(start=start, end=end,instrument=instrument)
+            # get name of observatory and type of instrument
+            ar = self.ar_instruments.get().split('/')
+            sp = self.sp_instruments.get().split('/')
+            fil = self.fil_instruments.get().split('/')
+
+            ar_obs = ar[0]
+            ar_instr = ar[1]
+            sp_obs = sp[0]
+            sp_instr = sp[1]
+            fil_obs = fil[0]
+            fil_instr = fil[1]
+
+            if len(start) == 0 or len(end) == 0 or len(ar) == 0 \
+                    or len(sp) == 0 or len(fil) == 0:
+                messagebox.showerror("Error", "Some of the values are empty!")
+            else:
+                create_map(start, end, ar_obs, ar_instr, sp_obs, sp_instr)
+
+        create_button = ttk.Button(container, text="Create Map", command=create)
+        create_button.grid(row=5, column=0)
 
 
-        createButton = ttk.Button(container, text="Create Map", command=create)
-        createButton.grid(row=5, column=0)
-
-
-def create_map(start, end, instrument):
-    data = DataAccess(start, end, 'AR')
-    chain_encoded = prep.encode_and_split(data.get_chain_code())
-    carr_synthesis, pix_synthesis = ar.get_shapes(chain_encoded, data.get_pixel_start_x(), data.get_pixel_start_y(),
+def create_map(start, end, ar_obs, ar_instr, sp_obs, sp_instr):
+    # setting active regions
+    data = DataAccess(start, end, 'AR', ar_obs, ar_instr)
+    chain_encoded = prep.decode_and_split(data.get_chain_code())
+    ar_carr_synthesis, ar_pix_synthesis = ar.get_shapes(chain_encoded, data.get_pixel_start_x(), data.get_pixel_start_y(),
                                                data.get_filename(),
                                                data.get_noaa_number(), data.get_ar_id(), data.get_date())
-    prep.display_object(carr_synthesis)
 
+    # setting sunspots
+    sp_data = DataAccess(start, end, 'SP', sp_obs, sp_instr)
+
+    sp_chain_encoded = prep.decode_and_split(sp_data.get_chain_code())
+
+    sp_carr, sp_pix = sp.get_shapes(sp_chain_encoded, sp_data.get_pixel_start_x(), sp_data.get_pixel_start_y(),
+                                sp_data.get_filename(), sp_data.get_sp_id(), sp_data.get_date())
+
+
+    sp_synthesis = sp.make_sp_synthesis(ar_contour=ar_carr_synthesis, sp_carr=sp_carr)
+
+    prep.display_object(ar_carr_synthesis, sp_synthesis)
 
 
 if __name__ == '__main__':

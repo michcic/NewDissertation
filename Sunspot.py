088b2e131cec493daf0478eaf8f2d35ed6e917da
varenghar <varenghar@users.noreply.github.com>
2018-04-09 14:59:37 +0100

    again

--- Sunspot.py
+++ Sunspot.py
@@ -1,19 +1,5 @@
 import numpy as np
-import matplotlib as plt
-from astropy.coordinates import SkyCoord
-import sunpy.map
-import matplotlib.pyplot as plt
-from sunpy.coordinates import frames
-import astropy.units as u
-import matplotlib
 from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-import math
-from descartes import PolygonPatch
-from PIL import Image
-import cv2
-import json
-import sqlite3
 import Database as db
 import ObjectPreparation as prep
 from shapely.geometry import Point
@@ -27,9 +13,8 @@ from shapely.geometry.polygon import Polygon
 # starty - y coordinate of chain code start position in pixels
 # return - array with coordinates of the contour of the object
 def get_shapes(chains, startx, starty, filename, sp_id, date):
-    print("get_shapes() START")
-    filename = prep.encode_filename(filename)
-    date = prep.encode_date(date)
+    filename = prep.decode_filename(filename)
+    date = prep.decode_date(date)
     all_coords_carr = []
     all_contours_pix = []
     counter = 0
@@ -39,23 +24,19 @@ def get_shapes(chains, startx, starty, filename, sp_id, date):
         xpos = startx[counter]  # Starting position of contour
         ypos = starty[counter]
         s_id = sp_id[counter]
-        print("ID", s_id)
         file = filename[counter]
         sp_date = date[counter]
         # Check if exists in database
         result = db.load_sp_from_database(s_id)
         if not result == ([], []):
-            print("RESULT NOT NULL")
             # check if object go through the end of map and finish at the beginning
             broken = (max(result[0][0][0]) - min(result[0][0][0])) > 358
             if not broken:
                 all_coords_carr += result[0]
                 all_contours_pix += (result[1])
         else:
-            print("RESULT NULL")
             # Calculate ar contour in pixel, carrington longitude and latitude
             sp_pix, lon, lat = prep.get_shape(coords=c, xpos=xpos, ypos=ypos, file=file)
-            print("pix", sp_pix)
             all_contours_pix.append(sp_pix)
             db.add_sunspot_to_database(sp_id=s_id, date=sp_date, carr_coords=[lon, lat], pix_coords=sp_pix)
 
@@ -84,12 +65,11 @@ def make_sp_synthesis(ar_contour, sp_carr):
             # must be stored this way:[(lon,lat),(lon,lat)...]
             ar_zip = list(zip(ar[0], ar[1]))
             ar = Polygon(np.array(ar_zip))
-            print("AR in SP SYN", ar)
+
             result = []
             for p in sp_zip:  # go through each point of sp
                 p = Point(p[0], p[1])
                 test = ar.contains(p)  # Return true if point is inside of the polygon
-                print(test)
                 if test:
                     result.append(test)
 
@@ -98,10 +78,8 @@ def make_sp_synthesis(ar_contour, sp_carr):
             proportion = len(result)/len(sp_zip)
             if proportion == 1.0:
                 sunspots.append(sp)
-                print("ADDED")
                 break
-            print("Proportion:", len(result)/len(sp_zip))
-    print("Sunspots len", len(sunspots))
+
     return sunspots
 
 
@@ -112,7 +90,7 @@ if __name__ == '__main__':
 
     # setting active regions
     data = DataAccess('2003-10-21T00:00:00', '2003-10-24T00:00:00', 'AR', 'SOHO', 'MDI')
-    chain_encoded = prep.encode_and_split(data.get_chain_code())
+    chain_encoded = prep.decode_and_split(data.get_chain_code())
     ar_carr_synthesis, ar_pix_synthesis = ar.get_shapes(chain_encoded, data.get_pixel_start_x(),
                                                         data.get_pixel_start_y(),
                                                data.get_filename(),
@@ -121,7 +99,7 @@ if __name__ == '__main__':
     # setting sunspots
     sp_data = DataAccess('2003-10-21T00:00:00', '2003-10-24T00:00:00', 'SP', 'SOHO', 'MDI')
 
-    sp_chain_encoded = prep.encode_and_split(sp_data.get_chain_code())
+    sp_chain_encoded = prep.decode_and_split(sp_data.get_chain_code())
 
     sp_carr, sp_pix = get_shapes(sp_chain_encoded, sp_data.get_pixel_start_x(),
                                  sp_data.get_pixel_start_y(),

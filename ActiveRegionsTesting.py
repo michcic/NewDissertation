b3d5817d81feded438253fe9dbccc625dee85375
varenghar <varenghar@users.noreply.github.com>
2018-04-25 17:26:20 +0100

    ST

--- ActiveRegionsTesting.py
+++ ActiveRegionsTesting.py
@@ -1,20 +1,28 @@
 from DataAccess import DataAccess
 import ObjectPreparation as prep
-import ActiveRegion as ar
-import numpy as np
 import Database as db
 
 
+# Functions test Carrington position of active regions
+# against gravity center values from HFC database
 def test_active_regions(ar_id, grav_long, grav_lat):
     fail = 0
     success = 0
+    ignored = 0
     for id, long, lat in zip(ar_id, grav_long, grav_lat):
         db_result = db.load_ar_from_database(id)
         carr_coords = db_result[2]
 
+        # Before testing the object three conditions are checked
+        # 1. check if object go through the end of map and finish at the beginning
+        # if yes, then ignore
+        # Check if gavity_longitude and grav_latitude values from HFC database are valid i.e they
+        # are withing appropriate range (long: 0 to 360 and lat: -90 to 90)
         broken = (max(carr_coords[0][0]) - min(carr_coords[0][0])) > 358
+        valid_long = 0 <= long <= 360
+        valid_lat = -90 <= lat <= 90
 
-        if not broken:
+        if not broken and valid_lat and valid_long:
             x = carr_coords[0][0]
             y = carr_coords[0][1]
             centroid = (sum(x) / len(x), sum(y) / len(y))
@@ -24,6 +32,9 @@ def test_active_regions(ar_id, grav_long, grav_lat):
             width = max(carr_coords[0][0]) - min(carr_coords[0][0])
             height = max(carr_coords[0][1]) - min(carr_coords[0][1])
 
+            # if the longitude difference is smaller than width of ar
+            # and latitude is smaller than height of ar then is success
+            # otherwise fail
             if lon_diff < width and lat_diff < height:
                 print("SUCCESS", lon_diff)
                 success += 1
@@ -33,9 +44,12 @@ def test_active_regions(ar_id, grav_long, grav_lat):
                 print("calculated:", centroid[0])
                 fail += 1
                 # prep.display_object(carr_coords, [])
+        else:
+            ignored += 1
 
     print("successes = ", success)
     print("fail = ", fail)
+    print("ignored = ", ignored)
 
 
 # Active regions position testing
